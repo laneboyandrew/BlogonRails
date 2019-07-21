4b14b664aa6dcc9da75ede6d979dd38deb325f60
Andrey Grach <andrejgrach@icloud.com>
2019-07-21 22:55:04 +0300

    Add "contact" model validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,14 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    @contact.save
+    if @contact.valid?
+      @contact.save
+    else
+      #т.к. возвращает на текущую стр без метода get необходимо сделать ренд на /contact/new т.к. там есть get
+      render action: 'new'
+      end
+    end
+
   end
 
   private
@@ -13,4 +20,4 @@ class ContactsController < ApplicationController
   def contact_params
     params.require(:contact).permit(:email, :message)
   end
-end
+
